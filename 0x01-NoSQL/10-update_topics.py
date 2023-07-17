13a1d1dca5cd5fd0d91a2972b0d86362d5bf016a
Torpeedofv <torpeedo.fv@gmail.com>
2023-07-17 21:51:51 +0100

    task 10 correction

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,4 +7,4 @@ import pymongo
 def update_topics(mongo_collection, name, topics):
     """A function that changes all topics of a school document
     based on the name"""
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
