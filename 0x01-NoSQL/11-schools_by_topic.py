8e63d7f5e57912682394d0439e4cd03a9170d4b6
Torpeedofv <torpeedo.fv@gmail.com>
2023-07-17 22:03:32 +0100

    task 11 correction

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,4 +6,4 @@ import pymongo
 
 def schools_by_topic(mongo_collection, topic):
     """A function that returns the list of school having a specific topic"""
-    return mongo_collection.find({"topic": topic})
+    return mongo_collection.find({"topics": topic})
