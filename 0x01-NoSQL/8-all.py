d6e01fb3f32349f156cbfaa598219e2da5fa35c0
Torpeedofv <torpeedo.fv@gmail.com>
2023-07-17 21:04:28 +0100

    tasks 1 - 7

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A function that list all documents in a collection"""
+
+import pymongo
+def list_all(mongo_collection):
+    if mongo_collection is None:
+        return []
+    return mongo_collection.find()
