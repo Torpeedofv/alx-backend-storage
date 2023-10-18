43e746b637f3f7b73c731f8c9f80729058f87c3d
Torpeedofv <torpeedo.fv@gmail.com>
2023-10-18 07:44:25 +0100

    task 7 redo

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,13 +3,13 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-	DECLARE total_score FLOAT;
-	DECLARE total_projects INT;
+	DECLARE total_score FLOAT DEFAULT 0;
+	DECLARE total_projects INT DEFAULT 0;
 	SELECT SUM(score), COUNT(*) INTO total_score, total_projects
-	FROM corrections WHERE user_id = user_id;
+	FROM corrections WHERE corrections.user_id = user_id;
 
 	UPDATE users
-	SET users.average_score = IF(total_projects = 0, 0, total_score / total_projects)
+	SET users.average_score = total_score / total_projects
 	WHERE users.id = user_id;
 END //
 DELIMITER ;
