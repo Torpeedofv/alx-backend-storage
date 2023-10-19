fc1937c38abb544d6040d5d44d40a30ef359c19c
Torpeedofv <torpeedo.fv@gmail.com>
2023-10-19 21:50:18 +0100

    task 5

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""module to implement a function prototype"""
+from typing import Callable
+from functools import wraps
+import redis
+import requests
+redis_client = redis.Redis()
+
+
+def counter(method: Callable) -> Callable:
+    """counts the number of times the url is accessed"""
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        url = args[0]
+        redis_client.incr(f"count:{url}")
+        cached = redis_client.get(f'{url}')
+        if cached:
+            return cached.decode('utf-8')
+        response = method(url)
+        redis_client.setex(url, 10, response)
+        return response
+    return wrapper
+
+
+@counter
+def get_page(url: str) -> str:
+    """get a page and cache value"""
+    response = requests.get(url)
+    return response.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
